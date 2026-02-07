35e9ced80632803763a67fb48c03324830cb3fb1
Aaronc0d3 <zaman.jarif1@gmail.com>
2026-02-06 20:58:01 -0500

    Intial commit

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,84 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "williamboman/mason.nvim",
+    "jay-babu/mason-nvim-dap.nvim",
+  },
+  config = function ()
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    require("dapui").setup()
+
+    --C++ debugger adapter
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = "${port}",
+      executable = {
+
+        command = vim.fn.stdpath("data") .. '/mason/bin/codelldb',
+        args = {"--port", "${port}"},
+      }
+    }
+
+    dap.configurations.cpp = {
+      {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+      },
+    }
+
+    dap.adapters.python = {
+      type = 'executable',
+      command = vim.fn.stdpath("data") .. '/mason/packages/debugpy/venv/bin/python',
+      args = { '-m', 'debugpy.adapter' },
+    }
+
+    --python adapter
+
+    dap.configurations.python = {
+      {
+        type = 'python',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+          -- Use virtual environment if available
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+            return cwd .. '/venv/bin/python'
+          elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+            return cwd .. '/.venv/bin/python'
+          else
+            return '/usr/bin/python3'
+          end
+        end,
+      },
+    }
+
+    dap.configurations.c = dap.configurations.cpp
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
+
+    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+  end,
+}
